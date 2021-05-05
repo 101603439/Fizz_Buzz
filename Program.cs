5ca4a05566fc9d874e5719ca83bec5bef538b9eb
101603439 <101603439@student.swin.edu.au>
2021-05-06 06:32:25 +1000

    Fizz_Buzz

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SelectionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string results = "";
+        for (int j = 1; j <= 100; j++)
+        {
+            if ( j % 3 == 0) 
+            results = "Fizz ";
+
+            if ( j % 5 == 0) 
+            results = results + "Buzz";
+
+            if (results.Length == 0) results = j.ToString();
+            
+            Console.WriteLine(results);
+        }
+        }
+    }
+}
